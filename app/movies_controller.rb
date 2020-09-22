91f01dadd96f14bb7b7575947a7fc57a96208baf
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-22 19:19:53 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  end
+
 end
 
 def can_get_the_first_item_in_the_database
