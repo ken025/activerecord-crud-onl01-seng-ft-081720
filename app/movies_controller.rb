dd9a30b1462a959fb06ac7001c935ccdaf6f8d26
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-22 19:19:42 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,9 +28,6 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  
-  Movie.create do |m|
-    __
   end
 end
 
